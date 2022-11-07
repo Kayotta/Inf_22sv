330c3ea678737c710b38389115e39e092a0b5502
FameUkulele <vera.senn@stud.edubs.ch>
2022-11-07 12:23:01 +0100

    Algorithmus

---
+++ Algorithmus.py
@@ -0,0 +1,51 @@
+import time
+import random
+
+#Variablen
+figuren = ('Schere', 'Stein', 'Papier')
+spielen = True
+
+while spielen:
+
+    #Spielfigur auswählen
+    spielerauswahl = 0
+    while spielerauswahl not in (1,2,3):
+        spielerauswahl = int(input("[1]Schere [2]Stein [3]Papier/n"))
+    spielerfigur = figuren[spielerauswahl - 1]
+
+    #Computerfigur auswählen
+    computerfigur = figuren[random.randint(0, 2)]
+
+    #Sieger ermitteln
+    if spielerfigur == computerfigur:
+        print("Unentschieden")
+    else:
+        if spielerfigur == "Schere":
+            if computerfigur == "Stein":
+                print("Verloren")
+            else:
+                print("Gewonnen")
+
+        if spielerfigur == "Stein":
+            if computerfigur == "Papier":
+                print("Verloren")
+            else:
+                print("Gewonnen")
+
+        if spielerfigur == "Papier":
+            if computerfigur == "Schere":
+                print("Verloren")
+            else:
+                print("Gewonnen")
+
+    #Restart
+    time.sleep(1)
+    entscheidung = ""
+    while entscheidung not in ("y", "n"):
+        entscheidung = input ("/nNochmal spielen? [y]Ja [n]Nein")
+
+    if entscheidung == "n":
+        spielen = False
+
+
+
