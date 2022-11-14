531922e0dbd8242a332011152892a3c1f29da956
Kayotta <sophie.petry@stud.edubs.ch>
2022-11-14 12:04:51 +0100

    display Farbveränderung

--- Grafisches Design.py
+++ Grafisches Design.py
@@ -6,10 +6,11 @@ pygame.init()
 display = pygame.display.set_mode((800, 600))
 
 while True:
-    display.fill((0, 0, 0))
+    display.fill((255, 255, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
             pygame.QUIT
 
+    pygame.display.update()
