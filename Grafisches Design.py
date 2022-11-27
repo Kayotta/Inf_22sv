0dd1b040dad1abbdbc14f6575d9530de5420bbe8
FameUkulele <vera.senn@stud.edubs.ch>
2022-11-27 22:05:01 +0100

    Grafisches Design: Icon und Titel können geändert werden

--- Grafisches Design.py
+++ Grafisches Design.py
@@ -2,9 +2,15 @@ import pygame
 import sys
 pygame.init()
 
-
+#Display erstellen
 display = pygame.display.set_mode((800, 600))
 
+#Icon und Titel ändern
+pygame.display.set_caption("Rock-Paper-Scissors")
+'''icon = pygame.image.load('')
+pygame.display.set_icon(icon)'''
+
+#Spielloop erstellen
 while True:
     display.fill((255, 255, 0))
 
