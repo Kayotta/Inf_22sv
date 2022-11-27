6fa02ca79ef046b7049bb0aaa67cf55cf9dc0e92
FameUkulele <vera.senn@stud.edubs.ch>
2022-11-27 21:44:41 +0100

    Grafisches Design: Hintergrundfarben können jetzt geändert werden.

--- Grafisches Design.py
+++ Grafisches Design.py
@@ -1,15 +1,24 @@
 import pygame
+import os
 import sys
 pygame.init()
 
 
+#Display erstellen
 display = pygame.display.set_mode((800, 600))
 
-while True:
-    display.fill((0, 0, 0))
+#Titel und Icon des Displays
+pygame.display.set_caption("Rock-Paper-Scissors")
+'''icon = pygame.image.load('rock-paper-scissors.png')
+pygame.display.set_icon(icon)'''
 
+#Game loop erstellen
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
             pygame.QUIT
 
+
+    display.fill((0,155,0))
+    pygame.display.update()
